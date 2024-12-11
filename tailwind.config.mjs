ec51420405ff2e855c3f26773128dc21f452bbbb
Huijin <huba0001@stud.kea.dk>
2024-12-11 14:29:21 +0100

    config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,156 @@
-const plugin = require('tailwindcss/plugin');
-
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 
-
-	theme: {
-
-        extend: {
-            colors: {
-              
-            },
-
-            
-          },
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-
-        ],
-      }
-}
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      orange: {
+        50: "#fff7ec",
+        100: "#ffedd3",
+        200: "#ffd8a5",
+        300: "#ffbb6d",
+        400: "#ff9232",
+        500: "#ff730a",
+        600: "#ff5900",
+        700: "#cc3e02",
+        800: "#a1310b",
+        900: "#822b0c",
+        950: "#461204",
+      },
+      blue: {
+        50: "#e8f1ff",
+        100: "#d5e4ff",
+        200: "#b3ccff",
+        300: "#85a8ff",
+        400: "#5676ff",
+        500: "#2f45ff",
+        600: "#0c0eff",
+        700: "#0000fe",
+        800: "#0609cd",
+        900: "#10169f",
+        950: "#0a0b5c",
+      },
+      gray: {
+        50: "#f6f7f9",
+        100: "#edeef1",
+        200: "#d6d9e1",
+        300: "#b2b9c7",
+        400: "#8893a8",
+        500: "#69758e",
+        600: "#545e75",
+        700: "#454d5f",
+        800: "#393f4d",
+        900: "#353945",
+        950: "#23262e",
+      },
+      neutral: {
+        50: "#f7f7f7",
+        100: "#ededed",
+        200: "#d4d4d4",
+        300: "#c8c8c8",
+        400: "#adadad",
+        500: "#999999",
+        600: "#888888",
+        700: "#7b7b7b",
+        800: "#676767",
+        900: "#545454",
+        950: "#363636",
+      },
+    },
+    spacing: {
+      none: "0px",
+      xxxxs: "2px",
+      xxxs: "4px",
+      xxs: "8px",
+      xs: "16px",
+      s: "24px",
+      sm: "32px",
+      m: "40px",
+      md: "48px",
+      ml: "56px",
+      lg: "64px",
+      xl: "80px",
+      "2xl": "96px",
+      "3xl": "112px",
+      "4xl": "128px",
+      "5xl": "144px",
+      "6xl": "160px",
+      "7xl": "176px",
+      "8xl": "192px",
+      "9xl": "208px",
+      "10xl": "224px",
+      "11xl": "320px",
+      "12xl": "360px",
+      "13xl": "424px",
+    },
+    fontFamily: {
+      lato: ["Lato", "sans-serif"],
+      display: ["Titillium Web", "sans-serif"],
+    },
+    fontSize: {
+      konsultativ: ["0.688rem", { lineHeight: "1.0rem" }],
+      body: ["0.813rem", { lineHeight: "1.25rem" }],
+      base: ["1rem", { lineHeight: "1.5rem" }],
+      small: ["1.5rem", { lineHeight: "2.25rem" }],
+      medium: ["2rem", { lineHeight: "3rem" }],
+      large: ["2.5rem", { lineHeight: "3.75rem" }],
+      xlarge: ["3rem", { lineHeight: "4.5rem" }],
+      "display-desktop": ["5rem", { lineHeight: "5.5rem" }],
+      "display-mobile": ["2.5rem", { lineHeight: "3.5rem" }],
+      "h1-desktop": ["3rem", { lineHeight: "3.75rem" }],
+      "h1-mobile": ["2.2rem", { lineHeight: "3.125rem" }],
+      "h2-desktop": ["2.5rem", { lineHeight: "3.125rem" }],
+      "h2-mobile": ["2rem", { lineHeight: "2.5rem" }],
+      "h3-desktop": ["2rem", { lineHeight: "2.5rem" }],
+      "h3-mobile": ["1.5rem", { lineHeight: "2.25rem" }],
+      "h4-desktop": ["1.25rem", { lineHeight: "2.25rem" }],
+      "h4-mobile": ["1.125rem", { lineHeight: "1.875rem" }],
+      "p-desktop": ["1.125rem", { lineHeight: "1.875rem" }],
+      "p-mobile": ["1rem", { lineHeight: "1.5rem" }],
+      "a-desktop": ["1.125rem", { lineHeight: "1.875rem" }],
+      "a-mobile": ["1rem", { lineHeight: "1.5rem" }],
+    },
+    fontWeight: {
+      light: "300",
+      regular: "400",
+      semibold: "600",
+      bold: "700",
+      black: "900",
+    },
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0px",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+    },
+    borderRadius: {
+      DEFAULT: "1.25rem",
+      none: "0px",
+      s: "5px",
+      sm: "10px",
+      md: "30px",
+      lg: "40px",
+      full: "9999px",
+    },
+    boxShadow: {
+      drop25: "4px 4px 4px 0 rgb(0 0 0 / 0.25)",
+      drop50: "4px 4px 4px 0 rgb(0 0 0 / 0.50)",
+      inner25: "0 4px 4px 0 rgb(0 0 0 / 0.25) inset",
+      inner50: "0 4px 4px 0 rgb(0 0 0 / 0.50) inset",
+    },
+    extend: {
+      screens: {
+        "custom-xl": "1280px", // Custom breakpoint for 1280px
+        "custom-lg": "1030px", // Custom breakpoint for 1030px
+        "custom-md": "982px", // Custom breakpoint for 982px
+        "custom-sm": "768px", // Custom breakpoint for 768px
+      },
+      tablet: "768px", // Define your tablet breakpoint
+      desktop: "1024px", // Define your desktop breakpoint
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+};
