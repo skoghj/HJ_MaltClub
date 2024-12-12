88bf5af44fa5ce3b831c6d96f1869e54af47ef43
Nikolaj <nikolajdcg@gmail.com>
2024-12-12 23:32:36 +0100

    goodchanges

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    extend: {
     colors: {
       transparent: "transparent",
       current: "currentColor",
@@ -140,3 +141,4 @@ export default {
       require('@tailwindcss/container-queries')
     ],
   }
+}
