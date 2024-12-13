25f7e99359fd40a6f85613c67ff259f75441cb14
Nikolaj <nikolajdcg@gmail.com>
2024-12-13 11:59:13 +0100

    config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    extend: {
     colors: {
       transparent: "transparent",
       current: "currentColor",
@@ -139,4 +140,4 @@ export default {
       require('@tailwindcss/aspect-ratio'),
       require('@tailwindcss/container-queries')
     ],
-  }
+  }}
